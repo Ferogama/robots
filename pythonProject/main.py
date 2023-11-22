e716583186a0be9e308f57f0310b3eb32b57e038
Радмир <90097099+Ferogama@users.noreply.github.com>
2023-11-22 20:58:48 +0300

    done first image

---
+++ pythonProject/main.py
@@ -0,0 +1,13 @@
+import cv2
+
+#Картинка под нмоером 0
+image = cv2.imread("C:/Users/fero/Desktop/12/0.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+_, binary = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY_INV)
+
+contours, _ = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+contour_image = image.copy()
+cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
+cv2.imwrite("inner_contours.jpg", contour_image)
+
+
