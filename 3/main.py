8298f08b2e364abf475f10185ae00aff566e97ba
Радмир <90097099+Ferogama@users.noreply.github.com>
2023-11-28 23:29:56 +0300

    wf

--- 3/main.py
+++ 3/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread("C:/Users/fero/Desktop/12/3.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-_, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+_, binary = cv2.threshold(gray, 90, 255, cv2.THRESH_BINARY)
 
 counters, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 image = cv2.drawContours(image, counters, -1, (0,255,0), 2)
